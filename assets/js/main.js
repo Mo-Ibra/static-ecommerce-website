09952f33d05e7a805242c303a5a3902f35e4bef0
Dev-Maradona <maradona.0000@yahoo.com>
2022-09-14 09:35:50 -0700

    Added Menu Navigation Bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,62 @@
 const notificationToast = document.querySelector('[data-toast]');
 const toastCloseBtn = document.querySelector('[data-toast-close]');
 
-toastCloseBtn.addEventListener('click', function() {
+toastCloseBtn.addEventListener('click', function () {
     notificationToast.classList.add('closed');
 });
+
+
+// Mobile Menu Variables
+
+const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
+const mobileMenu = document.querySelector('[data-mobile-menu]');
+const mobileMenuCloseBtn = document.querySelector('[data-mobile-menu-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
+
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+
+    // mobile menu function
+    const mobileMenuCloseFunc = function () {
+        mobileMenu.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+
+    mobileMenuOpenBtn[i].addEventListener('click', function () {
+        mobileMenu.classList.add('active');
+        overlay.classList.add('active');
+    });
+
+    mobileMenuCloseBtn.addEventListener('click', mobileMenuCloseFunc);
+    overlay.addEventListener('click', mobileMenuCloseFunc);
+
+}
+
+// Accordion variables
+const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
+const accordion = document.querySelectorAll('[data-accordion]');
+
+for (let i = 0; i < accordionBtn.length; i++) {
+
+    accordionBtn[i].addEventListener('click', function () {
+
+        const clickedBtn = this.nextElementSibling.classList.contains('active');
+
+        for (let i = 0; i < accordion.length; i++) {
+
+            if (clickedBtn) break;
+
+            if (accordion[i].classList.contains('active')) {
+
+                accordion[i].classList.remove('active');
+                accordionBtn[i].classList.remove('active');
+
+            }
+
+        }
+
+        this.nextElementSibling.classList.toggle('active');
+        this.classList.toggle('active');
+
+    });
+
+}

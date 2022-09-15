a6c2146398f69609970e84db2b9b82f63a427d74
Dev-Maradona <maradona.0000@yahoo.com>
2022-09-15 08:58:44 -0700

    Added Sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,24 +16,24 @@ toastCloseBtn.addEventListener('click', function () {
 // Mobile Menu Variables
 
 const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
-const mobileMenu = document.querySelector('[data-mobile-menu]');
-const mobileMenuCloseBtn = document.querySelector('[data-mobile-menu-close-btn]');
+const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
+const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
 const overlay = document.querySelector('[data-overlay]');
 
 for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
 
     // mobile menu function
     const mobileMenuCloseFunc = function () {
-        mobileMenu.classList.remove('active');
+        mobileMenu[i].classList.remove('active');
         overlay.classList.remove('active');
     }
 
     mobileMenuOpenBtn[i].addEventListener('click', function () {
-        mobileMenu.classList.add('active');
+        mobileMenu[i].classList.add('active');
         overlay.classList.add('active');
     });
 
-    mobileMenuCloseBtn.addEventListener('click', mobileMenuCloseFunc);
+    mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
     overlay.addEventListener('click', mobileMenuCloseFunc);
 
 }
